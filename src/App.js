bd41ca6e9a274cf4d9548f932fd3018ac4bb2519
Pratik Deshpande <pratikdeshpande65@gmail.com>
2022-09-10 15:23:56 +0530

    Fixed Duplicate Cards and added delete card functionality

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class CardList extends React.Component {
 				<div className="card-deck row">
 					{this.props.profiles.length == 0 ? 
 						<img style={{ maxWidth: '500px', maxHeight: '500px', margin: '0 auto' }} src={background} className="img-fluid" alt="No users"/>  : 
-						this.props.profiles.map(profile => <Card key={profile.id} {...profile} />)
+						this.props.profiles.map(profile => <Card removeCard={this.props.removeCard} profiles={this.props.profiles} key={profile.id} {...profile} />)
 					}
 				</div>
 			</div>
@@ -22,20 +22,19 @@ class CardList extends React.Component {
 class Card extends React.Component {
 	render() {
 		const profile = this.props;
+		console.log(profile);
 
 		return (
 			<div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
-				<div className="card mb-4 shadow p-2 bg-white rounded">
+				<div className="card mb-4 p-2 bg-white rounded">
 					<div className="view overlay">
 						<img className="card-img-top" src={profile.avatar_url} alt="Card image cap" />
-						<a href="#!">
-							<div className="mask rgba-white-slight"></div>
-						</a>
 					</div>
 
 					<div className="card-body">
 						<h4 className="card-title">{profile.name}</h4>
 						<span className="card-text">User since <span style={{ color: 'darkgreen' }}>{profile.created_at.slice(0,10)}</span></span>
+						<hr/>
 						<p className="card-text" style={{fontFamily: 'sans-serif'}}>
 							<a style={{ textDecoration: 'none', color: 'grey' }} target="_blank" href={`${profile.html_url}?tab=followers`}>
 								<i className="fa fa-users"></i> {profile.followers} followers . 
@@ -44,9 +43,10 @@ class Card extends React.Component {
 								<i className="fa fa-person-walking"></i> {profile.following} following 
 							</a>
 						</p>
-						<span className="card-text" style={{ color: '#8500ff' }}><i class="fa fa-folder"></i> {profile.public_repos} repos</span>
+						<span className="card-text" style={{ color: '#8500ff' }}><i className="fa fa-folder"></i> {profile.public_repos} repos</span>
 						<p className="card-text">{profile.company ? `Works at ${profile.company}` : ''}</p>
 						<a target="_blank" href={profile.html_url} className="btn btn-dark"><i className="fa fa-github"></i> Profile</a>
+						<button style={{ marginLeft: '10px' }} onClick={() => this.props.removeCard(profile.id)} className="btn btn-danger"><i class="fa fa-trash"></i></button>
 					</div>
 				</div>
 			</div>
@@ -69,8 +69,17 @@ class Form extends React.Component {
 		if(err.code == 'ERR_BAD_REQUEST'){
 			alert('Invalid Username!');
 		} else {
-			this.props.onSubmit(resp.data);
-			console.log(resp.data);
+
+			// To check if user is already added
+			const hasCard = this.props.profiles.some(function(card) {
+				return card.id === resp.data.id;
+			});
+			
+			if(hasCard){
+				alert('User already added!');
+			} else {
+				this.props.onSubmit(resp.data);
+			}
 		}
 
 		this.setState({ userName: '' });
@@ -78,7 +87,7 @@ class Form extends React.Component {
 
 	render() {
   	return (
-    	<form onSubmit={this.handleSubmit}>
+    	<form id="head" onSubmit={this.handleSubmit}>
     	  	<input 
 				type="text" 
 				placeholder="GitHub username" 
@@ -102,12 +111,19 @@ class App extends React.Component {
 		}));
 	};
 
+	removeCard = (cardId) => {
+		const newProfiles = this.state.profiles.filter( obj => obj.id !== cardId);
+		this.setState(prevState => ({
+			profiles: newProfiles
+		}));
+	};
+
 	render() {
 		return (
 			<div>
 				<div className="header">{this.props.title}</div>
-				<Form onSubmit={this.addNewProfile} />
-				<CardList profiles={this.state.profiles} />
+				<Form onSubmit={this.addNewProfile} profiles={this.state.profiles}/>
+				<CardList removeCard={this.removeCard} profiles={this.state.profiles} />
 			</div>
 		);
 	}
