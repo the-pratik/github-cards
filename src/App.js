ffbc6c7226428b70579b312b4ce665bb18a0e339
Pratik Deshpande <pratikdeshpande65@gmail.com>
2022-09-29 14:33:35 +0530

    -r unused files

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import React from 'react';
 import axios from 'axios';
 
@@ -9,7 +8,7 @@ class CardList extends React.Component {
 		return (
 			<div className="container">
 				<div className="card-deck row">
-					{this.props.profiles.length == 0 ?
+					{this.props.profiles.length === 0 ?
 						<img style={{ maxWidth: '500px', maxHeight: '500px', margin: '0 auto' }} src={background} className="img-fluid" alt="No users" /> :
 						this.props.profiles.map(profile => <Card removeCard={this.props.removeCard} profiles={this.props.profiles} key={profile.id} {...profile} />)
 					}
@@ -27,7 +26,7 @@ class Card extends React.Component {
 			<div className="col-xs-12 col-sm-6 col-md-4 col-lg-3">
 				<div className="card mb-4 p-2 bg-white rounded">
 					<div className="view overlay">
-						<img className="card-img-top" src={profile.avatar_url} alt="Card image cap" />
+						<img className="card-img-top" src={profile.avatar_url} alt="Card Cap" />
 					</div>
 
 					<div className="card-body">
@@ -35,16 +34,16 @@ class Card extends React.Component {
 						<span className="card-text">User since <span style={{ color: 'darkgreen' }}>{profile.created_at.slice(0, 10)}</span></span>
 						<hr />
 						<p className="card-text" style={{ fontFamily: 'sans-serif' }}>
-							<a style={{ textDecoration: 'none', color: 'grey' }} target="_blank" href={`${profile.html_url}?tab=followers`}>
+							<a style={{ textDecoration: 'none', color: 'grey' }} target="_blank" rel="noreferrer" href={`${profile.html_url}?tab=followers`}>
 								<i className="fa fa-users"></i> {profile.followers} followers .
 							</a>
-							<a style={{ textDecoration: 'none', color: 'grey' }} target="_blank" href={`${profile.html_url}?tab=following`}>
+							<a style={{ textDecoration: 'none', color: 'grey' }} target="_blank" rel="noreferrer" href={`${profile.html_url}?tab=following`}>
 								<i className="fa fa-person-walking"></i> {profile.following} following
 							</a>
 						</p>
 						<span className="card-text" style={{ color: '#8500ff' }}><i className="fa fa-folder"></i> {profile.public_repos} repos</span>
 						<p className="card-text">{profile.company ? `Works at ${profile.company}` : ''}</p>
-						<a target="_blank" href={profile.html_url} className="btn btn-dark"><i className="fa fa-github"></i> Profile</a>
+						<a target="_blank" rel="noreferrer" href={profile.html_url} className="btn btn-dark"><i className="fa fa-github"></i> Profile</a>
 						<button style={{ marginLeft: '10px' }} onClick={() => this.props.removeCard(profile.id)} className="btn btn-danger"><i className="fa fa-trash"></i></button>
 					</div>
 				</div>
@@ -65,7 +64,7 @@ class Form extends React.Component {
 			err = error.toJSON();
 		});
 
-		if (err.code == 'ERR_BAD_REQUEST') {
+		if (err.code === 'ERR_BAD_REQUEST') {
 			alert('Invalid Username!');
 		} else {
 
